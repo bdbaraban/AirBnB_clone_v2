b6cc5abde56ae1e57aec9a3146a1cba2b35ff5c3
Andrew Lindburg <atlindburg@gmail.com>
2019-04-02 00:31:40 +0000

    db storage taking in environamental variables to connect to database and also pep8ed the file

---
+++ models/engine/db_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module for db storage engine """
+from os import gentenv
+from sqlalchemy.orm import relationship
+from sqlalchemy import (create_engine)
+from models.base_model import Base, BaseModel
+from models.city import City
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
+
+
+class DBStorage:
+    """ Represent a database storage engine """
+
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")))

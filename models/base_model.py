b336f7927db3005c4d18e354cfc94308cbb6416a
Andrew Lindburg <atlindburg@gmail.com>
2019-04-02 00:18:28 +0000

    fixed pep8 errors in base model

--- models/base_model.py
+++ models/base_model.py
@@ -7,13 +7,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 Base = declarative_base()
 
+
 class BaseModel:
     """Defines the BaseModel class."""
 
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    
+
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
 

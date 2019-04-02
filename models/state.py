702a6e1609aca65f6559a5eb5bc4095b6f4f5b41
Andrew Lindburg <atlindburg@gmail.com>
2019-04-02 00:21:01 +0000

    Finalized the relationship between State objs and City Obj and pep8 checked state.py file

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from models.city import City
 from sqlalchemy import String, Column
 
 
@@ -11,8 +12,12 @@ class State(BaseModel):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref = "state", cascade = "delete")
+    cities = relationship("City",  backref="state", cascade="delete")
 
     @property
     def cities(self):
-        for 
+        city_list = []
+        for city in list(models.storage.all(City).vallues()):
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list

489a80779c3c98c2f40c98b20726d23ec0f62206
bdbaraban <375@holbertonschool.com>
2019-04-03 23:31:21 +0000

    Add delete cascade option to relationships

--- models/place.py
+++ models/place.py
@@ -56,7 +56,7 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
-    reviews = relationship("Review", backref="place")
+    reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity",
                              viewonly=False)
     amenity_ids = []

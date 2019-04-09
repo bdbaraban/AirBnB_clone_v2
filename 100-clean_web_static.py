26faf121e867e2c87b54b01a651a6289f31d58a7
bdbaraban <375@holbertonschool.com>
2019-04-09 16:01:34 +0000

    Handle 0 arguments explicitly passed

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -16,7 +16,7 @@ def do_clean(number=0):
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
-    number = 1 if number == 0 else int(number)
+    number = 1 if int(number) == 0 else int(number)
 
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]

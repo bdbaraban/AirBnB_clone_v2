1be630a5a9a343aca2ea414d7d994c8c054aea5f
bdbaraban <375@holbertonschool.com>
2019-04-09 02:51:38 +0000

    Upload fabfile to call do_pack and do_deploy together

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Fabfile to create and distribute an archive to a web server.
+
+do_pack = __import__("1-pack_web_static").do_pack
+do_deploy = __import__("2-do_deploy_web_static").do_deploy
+
+
+def deploy():
+    """Create and distribute an archive to a web server."""
+    file = do_pack()
+    if file is None:
+        return False
+    print(file)
+    print("here")
+    return do_deploy(file)

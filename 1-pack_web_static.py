5ef41503a65cb5a51ae9fbad71958625912b5246
bdbaraban <375@holbertonschool.com>
2019-04-09 02:51:22 +0000

    Only call mkdir command to create versions if it doesn't already exist

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # Fabfile to generates a .tgz archive from the contents of web_static.
+import os.path
 from datetime import datetime
 from fabric.api import local
 
@@ -7,11 +8,15 @@ from fabric.api import local
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
-    archive = "web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                   dt.month,
-                                                   dt.day,
-                                                   dt.hour,
-                                                   dt.minute,
-                                                   dt.second)
-    local("mkdir -p versions")
-    local("tar -cvzf versions/{} web_static".format(archive))
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file

38540f0e7b4ade224490151969c9534723c9521e
bdbaraban <375@holbertonschool.com>
2019-04-09 03:05:55 +0000

    Specify env hosts for web servers in script

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 # Fabfile to create and distribute an archive to a web server.
+from fabric.api import env
 
 do_pack = __import__("1-pack_web_static").do_pack
 do_deploy = __import__("2-do_deploy_web_static").do_deploy
+
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def deploy():

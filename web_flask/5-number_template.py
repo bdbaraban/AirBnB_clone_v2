9d3818b33cd54a215502cd3bc0f616274753cdd2
bdbaraban <375@holbertonschool.com>
2019-04-23 23:43:38 +0000

    Remove enabling of strip/lstrip - not necessary in this task

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -14,8 +14,6 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
 
 
 @app.route("/", strict_slashes=False)

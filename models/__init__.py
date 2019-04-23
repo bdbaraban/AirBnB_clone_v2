0fdc36b0f50d1e20cd2cc7e014ccab91fdb33d4b
bdbaraban <375@holbertonschool.com>
2019-04-23 02:15:42 +0000

    Update module-level documentation to describe dbstorage/filestorage instantiation condition

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""create a unique FileStorage instance for your application"""
+"""Instantiates a storage object.
+
+-> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
+   instantiates a database storage engine (DBStorage).
+-> Otherwise, instantiates a file storage engine (FileStorage).
+"""
 from os import getenv
 
 

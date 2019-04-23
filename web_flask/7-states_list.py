f835073f4e9f004ddc05f981bc67181cc21a094c
bdbaraban <375@holbertonschool.com>
2019-04-23 23:55:00 +0000

    Instantiate Flask app at top of module

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,8 +6,10 @@ Routes:
     /states_list: HTML page with a list of all State objects in DBStorage.
 """
 from models import storage
-from web_flask import app
+from flask import Flask
 from flask import render_template
+
+app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)

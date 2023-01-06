2f82ddcec7b0ad28aae4c67957d5d9e16d4dbb1c
Mkiige <mkiige77@gmail.com>
2023-01-06 18:07:40 +0300

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,20 @@
--- This script prepares a MySQL server for the project
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+# Connect to MySQL as the root user
+mysql -u root -p
+
+# Create the database if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+# Create the user if it does not already exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+
+# Grant all privileges on the hbnb_test_db database to the hbnb_test user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+# Grant SELECT privilege on the performance_schema database to the hbnb_test user
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+# Flush the privileges to make the changes effective
+FLUSH PRIVILEGES;
+
+# Disconnect from MySQL
+EXIT;
